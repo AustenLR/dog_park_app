0741a64e0026630096124e98bd6d51d7a1be711f
Austen Lane-Roberts <austen.lane.roberts@gmail.com>
2015-10-01 10:14:41 -0700

    updated index file

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-mongoose.connect( process.env.MONGOLAB_URI || "mongodb://localhost/YOUR_DB_NAME");
+mongoose.connect( process.env.MONGOLAB_URI || "mongodb://localhost/dog_park_app");
 mongoose.set("debug", true);
 
 module.exports.Message = require('./message');
